5913f6d18a71a3936f3ee19add5b37cb3ea9755c
鄧祖恩 <s369236s@gmail.com>
2021-09-08 16:42:01 +0800

    echo

--- travel-go-line-bot/app.js
+++ travel-go-line-bot/app.js
@@ -1,14 +1,48 @@
-import express from "express";
+"use strict";
+
+const line = require("@line/bot-sdk");
+const express = require("express");
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static("public"));
-
-app.get("/api", (req, res) => {
-  res.send("123");
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post("/callback", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
 });
 
-app.listen(PORT, () => {
-  console.log(`serve listened on ${PORT}`);
+// event handler
+function handleEvent(event) {
+  if (event.type !== "message" || event.message.type !== "text") {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: "text", text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
