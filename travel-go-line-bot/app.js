85f7c477e887eef76510078e2125454ca8d81635
呂允丞 <h1253567@gmail.com>
2021-09-08 22:27:24 +0800

    message event test

--- travel-go-line-bot/app.js
+++ travel-go-line-bot/app.js
@@ -10,6 +10,9 @@ const config = {
     "j/jEW9aJJtAJ/1LITYLXp84Up0QZt2uaf75CyxQylJ1FsgSfnsCoepYQX68AS5nEuo6vhbBKk1K5emzwjijRN6CGB0kFvdgC8SD2tnTI2iwi/OW22G3wLBytTmFOK2Mg9+4TpdgybIkw4rHGk/myDgdB04t89/1O/w1cDnyilFU=",
 };
 
+// base URL for webhook server
+let baseURL = process.env.BASE_URL;
+
 // create LINE SDK client
 const client = new line.Client(config);
 
@@ -37,16 +40,82 @@ app.get("/", async (_, res) => {
 
 // event handler
 function handleEvent(event) {
-  if (event.type !== "message" || event.message.type !== "text") {
-    // ignore non-text-message event
-    return Promise.resolve(null);
+  // if (event.type !== "message" || event.message.type !== "text") {
+  //   // ignore non-text-message event
+  //   return Promise.resolve(null);
+  // }
+
+  // // create a echoing text message
+  // const echo = { type: "text", text: event.message.text };
+
+  // // use reply API
+  // return client.replyMessage(event.replyToken, echo);
+  switch (event.type) {
+    case "message":
+      const message = event.message;
+      switch (message.type) {
+        case "text":
+          return handleText(message, event.replyToken, event.source);
+        case "image":
+          return handleImage(message, event.replyToken);
+        default:
+          throw new Error(`Unknown message: ${JSON.stringify(message)}`);
+      }
+    default:
+      throw new Error(`Unknown event: ${JSON.stringify(event)}`);
   }
 
-  // create a echoing text message
-  const echo = { type: "text", text: event.message.text };
+  function handleText(message, replyToken, source) {
+    const buttonsImageURL = `${baseURL}/static/buttons/1040.jpg`;
 
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
+    switch (message.text) {
+      case "profile":
+        if (source.userId) {
+          return client
+            .getProfile(source.userId)
+            .then((profile) =>
+              replyText(replyToken, [
+                `Display name: ${profile.displayName}`,
+                `Status message: ${profile.statusMessage}`,
+              ])
+            );
+        } else {
+          return replyText(
+            replyToken,
+            "Bot can't use profile API without user ID"
+          );
+        }
+      case "buttons":
+        return client.replyMessage(replyToken, {
+          type: "template",
+          altText: "Buttons alt text",
+          template: {
+            type: "buttons",
+            thumbnailImageUrl: buttonsImageURL,
+            title: "My button sample",
+            text: "Hello, my button",
+            actions: [
+              { label: "Go to line.me", type: "uri", uri: "https://line.me" },
+              {
+                label: "Say hello1",
+                type: "postback",
+                data: "hello こんにちは",
+              },
+              {
+                label: "言 hello2",
+                type: "postback",
+                data: "hello こんにちは",
+                text: "hello こんにちは",
+              },
+              { label: "Say message", type: "message", text: "Rice=米" },
+            ],
+          },
+        });
+      default:
+        console.log(`Echo message to ${replyToken}: ${message.text}`);
+        return replyText(replyToken, message.text);
+    }
+  }
 }
 
 // listen on port
