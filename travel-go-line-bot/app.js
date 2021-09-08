12db9a6ab089dd0bd115e69f4c6d2a974af25d8b
鄧祖恩 <s369236s@gmail.com>
2021-09-08 17:24:34 +0800

    echo

--- travel-go-line-bot/app.js
+++ travel-go-line-bot/app.js
@@ -5,8 +5,9 @@ const express = require("express");
 const p = 1;
 // create LINE SDK config from env variables
 const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: "e2c64317520995b9f048d68720467b95",
+  channelSecret:
+    "j/jEW9aJJtAJ/1LITYLXp84Up0QZt2uaf75CyxQylJ1FsgSfnsCoepYQX68AS5nEuo6vhbBKk1K5emzwjijRN6CGB0kFvdgC8SD2tnTI2iwi/OW22G3wLBytTmFOK2Mg9+4TpdgybIkw4rHGk/myDgdB04t89/1O/w1cDnyilFU=",
 };
 
 // create LINE SDK client
