c49a84d43c3fab1281f118285fbfb93dfcb537d8
鄧祖恩 <s369236s@gmail.com>
2021-09-08 17:17:54 +0800

    init

--- travel-go-line-bot/app.js
+++ travel-go-line-bot/app.js
@@ -2,7 +2,7 @@
 
 const line = require("@line/bot-sdk");
 const express = require("express");
-const p = 0;
+const p = 1;
 // create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
