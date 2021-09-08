64f436e08e36f64447a2410ba7169f3de2f1f255
鄧祖恩 <s369236s@gmail.com>
2021-09-08 16:01:50 +0800

    init

--- travel-go-line-bot/app.js
+++ travel-go-line-bot/app.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
+app.get("/api", (req, res) => {
+  res.send("123");
+});
+
 app.listen(PORT, () => {
   console.log(`serve listened on ${PORT}`);
 });
