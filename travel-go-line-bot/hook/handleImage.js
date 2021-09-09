4f05150f35e1de3037237ef542304f23c92a4d92
呂允丞 <h1253567@gmail.com>
2021-09-09 14:37:25 +0800

    123

--- travel-go-line-bot/hook/handleImage.js
+++ travel-go-line-bot/hook/handleImage.js
@@ -2,46 +2,51 @@ const client = require("../config/client");
 const path = require("path");
 const fs = require("fs");
 const handleImage = async (message, replyToken) => {
-  let getContent;
-  if (message.contentProvider.type === "line") {
-    const downloadPath = path.join(
-      path.resolve("./"),
-      "downloaded",
-      `${message.id}.jpg`
-    );
-    const previewPath = path.join(
-      path.resolve("./"),
-      "downloaded",
-      `${message.id}-preview.jpg`
-    );
+  try {
+    let getContent;
+    if (message.contentProvider.type === "line") {
+      const downloadPath = path.join(
+        path.resolve("./"),
+        "downloaded",
+        `${message.id}.jpg`
+      );
+      const previewPath = path.join(
+        path.resolve("./"),
+        "downloaded",
+        `${message.id}-preview.jpg`
+      );
 
-    getContent = downloadContent(message.id, downloadPath).then(
-      (downloadPath) => {
-        // ImageMagick is needed here to run 'convert'
-        // Please consider about security and performance by yourself
-        cp.execSync(
-          `convert -resize 240x jpeg:${downloadPath} jpeg:${previewPath}`
-        );
+      getContent = downloadContent(message.id, downloadPath).then(
+        (downloadPath) => {
+          // ImageMagick is needed here to run 'convert'
+          // Please consider about security and performance by yourself
+          cp.execSync(
+            `convert -resize 240x jpeg:${downloadPath} jpeg:${previewPath}`
+          );
 
-        return {
-          originalContentUrl:
-            path.resolve("./") + "/downloaded/" + path.basename(downloadPath),
-          previewImageUrl:
-            path.resolve("./") + "/downloaded/" + path.basename(previewPath),
-        };
-      }
-    );
-  } else if (message.contentProvider.type === "external") {
-    getContent = Promise.resolve(message.contentProvider);
-  }
+          return {
+            originalContentUrl:
+              path.resolve("./") + "/downloaded/" + path.basename(downloadPath),
+            previewImageUrl:
+              path.resolve("./") + "/downloaded/" + path.basename(previewPath),
+          };
+        }
+      );
+    } else if (message.contentProvider.type === "external") {
+      getContent = Promise.resolve(message.contentProvider);
+    }
 
-  return getContent.then(({ originalContentUrl, previewImageUrl }) => {
-    return client.replyMessage(replyToken, {
-      type: "image",
-      originalContentUrl,
-      previewImageUrl,
+    return getContent.then(({ originalContentUrl, previewImageUrl }) => {
+      return client.replyMessage(replyToken, {
+        type: "image",
+        originalContentUrl,
+        previewImageUrl,
+      });
     });
-  });
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 };
 
 function downloadContent(messageId, downloadPath) {
