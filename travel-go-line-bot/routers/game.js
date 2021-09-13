ded4f41420692bf9562e0a4b893b54d4b00ebfe8
1072030 <46060471+1072030@users.noreply.github.com>
2021-09-13 16:49:42 +0800

    asas

--- travel-go-line-bot/routers/game.js
+++ travel-go-line-bot/routers/game.js
@@ -12,17 +12,17 @@ const firestore = admin.firestore();
 const realtimeDatabase = admin.database();
 const location = [
   {
-    name: "seven-eleven",
+    name: "shop1",
     lat: 24.9775727,
     lon: 121.2749167,
   },
   {
-    name: "high school",
+    name: "shop2",
     lat: 24.9749737,
     lon: 121.2716158,
   },
   {
-    name: "85℃",
+    name: "shop3",
     lat: 24.9751251,
     lon: 121.2721376,
   },
@@ -38,21 +38,41 @@ router.post("/position", async (req, res) => {
       distance(req.body.lat, req.body.lon, location[i].lat, location[i].lon)
     );
   }
+  await realtimeDatabase //取得現階段使用者exp
+    .ref(`${req.body.userId}`)
+    .get()
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        tempExp = snapshot.val().exp;
+      } else {
+        console.log("Experience not found");
+      }
+    });
   for (let i = 0; i < dis.length; i++) {
-    if (Math.round(dis[i] * 1000) < 100) {
+    //有幾間點 與距離
+    if (Math.round(dis[i] * 1000) < 1000) {
       //小於10公尺顯示
-      const userExits = await firestore.collection("Shop").doc("shop1").get();
+      const userExits = await firestore
+        .collection("Shop")
+        .doc(`${location[i].name}`) //doc(`${location.name}`)
+        .get();
+      console.log(userExits.id);
+      const userData = userExits.data().userId;
+      if (userExits.data().userId.indexOf(req.body.userId) === -1) {
+        userData.push(req.body.userId);
+        await firestore
+          .collection("Shop")
+          .doc(`${location[i].name}`)
+          .update({ userId: userData });
+        await realtimeDatabase.ref(`${req.body.userId}`).set({
+          exp: (tempExp += 100), //req.body.price
+        });
+        console.log("add user");
+      } else {
+        console.log("已取得今日經驗值");
+      }
     }
   }
-  /* const docRef = await firestore //消費金額 經驗值增加
-    .collection("Shop")
-    .doc("shop1 ")
-    .collection(`${req.body.userId}`)
-    .doc("content")
-    .set({
-      cost: 1000,
-    });
-  console.log(docRef); */
   res.send("success");
 });
 router.get("/experience", async (req, res) => {
@@ -70,10 +90,42 @@ router.get("/experience", async (req, res) => {
 });
 router.post("/experience", async (req, res) => {
   //使用者在店家消費金錢
-  //傳入 店家Id & 金額 & 密碼
-  realtimeDatabase.ref(`${req.body.userId}`).set({
-    exp: 300, //req.body.price
+  //傳入 userId 店家Id & 金額 & 密碼
+  let tempExp = 0;
+  await realtimeDatabase
+    .ref(`${req.body.userId}`) //req.param.userId
+    .get()
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        tempExp = snapshot.val().exp;
+      } else {
+        console.log("no data");
+      }
+    });
+  const test = await firestore
+    .collection("Shop")
+    .where("name", "==", req.body.shopId)
+    .get();
+  test.forEach((doc) => {
+    console.log(doc.id, "=>", doc.data());
   });
-  res.send("success");
+  const shopData = await firestore
+    .collection("Shop")
+    .doc(`${req.body.shopId}`)
+    .get();
+  if (shopData.data().password === req.body.password) {
+    //密碼配對
+    tempExp = tempExp + parseInt(req.body.price);
+    await realtimeDatabase.ref(`${req.body.userId}`).set({
+      exp: tempExp, //req.body.price
+    });
+    res.send({
+      status: "success",
+      experience: "+ " + req.body.price,
+      message: "消費成功!",
+    });
+  } else {
+    res.send("password incorrect");
+  }
 });
 module.exports = router;
