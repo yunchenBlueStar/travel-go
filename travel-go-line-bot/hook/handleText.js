1d59f161be4210e15d64cabe10f45ae5b779361f
1072030 <46060471+1072030@users.noreply.github.com>
2021-09-14 11:08:34 +0800

    update

--- travel-go-line-bot/hook/handleText.js
+++ travel-go-line-bot/hook/handleText.js
@@ -9,15 +9,6 @@ const handleText = async (message, replyToken, source) => {
         quickReply: {
           // ②
           items: [
-            // {
-            //   type: "action", // ③
-            //   imageUrl: "https://example.com/sushi.png",
-            //   action: {
-            //     type: "message",
-            //     label: "Sushi",
-            //     text: "Sushi",
-            //   },
-            // },
             {
               type: "action",
               action: {
