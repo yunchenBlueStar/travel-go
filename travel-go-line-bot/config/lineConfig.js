0b321102beee30e210f34770f8e0c60d68f6eb4f
1072030 <46060471+1072030@users.noreply.github.com>
2021-09-15 13:41:03 +0800

    test

--- travel-go-line-bot/config/lineConfig.js
+++ travel-go-line-bot/config/lineConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  channelSecret: "d20aeebc1b2021a09343a85c60e254bf",
+  channelSecret: "U7617848f47286749eec2d3faa45f9a8e",
   channelAccessToken:
     "bnUx7j7FEWbL1YD8uDfEVdrtrDCTpKIEIHw6uol3wD9PfcnB3lfmPzrMfKHhjhU6JmB2X05dvDYom9xtadLQDAaYQPHdVOnRwr76pOEGY6Do+AOxE1ciaGKQVvKrfqiRM+a2o1qhTG77H4cy6gMXGQdB04t89/1O/w1cDnyilFU=",
 };
