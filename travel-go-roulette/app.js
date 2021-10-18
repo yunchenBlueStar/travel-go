8499b91836de2e7d83e087dd081b544b1247a81a
1072030 <46060471+1072030@users.noreply.github.com>
2021-10-18 12:07:30 +0800

    #update cors

--- travel-go-roulette/app.js
+++ travel-go-roulette/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 const handleEventRouter = require("./public/index");
 app.use("/", handleEventRouter);
-app.use(cors());
+
 // listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
