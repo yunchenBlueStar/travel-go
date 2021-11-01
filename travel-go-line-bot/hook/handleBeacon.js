507986549d38a8b38817a7cd0524b0efe149e10e
1072030 <46060471+1072030@users.noreply.github.com>
2021-11-01 15:44:36 +0800

    #update beacon test

--- travel-go-line-bot/hook/handleBeacon.js
+++ travel-go-line-bot/hook/handleBeacon.js
@@ -1,10 +1,14 @@
 const client = require("../config/client");
-
+const firestore = require("../config/travel-rego-firebase-adminsdk-5yu3d-60f544b0da.json");
 const handleBeacon = async (source, replyToken) => {
-  console.log(source.beacon);
-  return await client.replyMessage(replyToken, {
-    type: "text",
-    text: `hello`,
+  const firestoreData = await firestore.collection("Shop").get();
+  firestoreData.forEach(async (doc) => {
+    console.log(doc.data());
   });
+  //{ hwid: '0154a2ec89', type: 'enter', dm: '50' } beacon Info
+  //   return await client.replyMessage(replyToken, {
+  //     type: "text",
+  //     text: `hello`,
+  //   });
 };
 module.exports = handleBeacon;
