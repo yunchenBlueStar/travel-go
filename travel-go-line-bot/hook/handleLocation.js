40e3c7c875f27bce08a679ad102a12494aaaab69
呂允丞 <h1253567@gmail.com>
2021-11-01 14:49:14 +0800

    loaciton test

--- travel-go-line-bot/hook/handleLocation.js
+++ travel-go-line-bot/hook/handleLocation.js
@@ -1,21 +1,20 @@
 const handleLocation = async (message, replyToken) => {
-  const mapApiconfig = {
-    token: "AIzaSyAee8zZhNBTeGuHNIClNS7ZSPqxG-a3Wgo",
-  };
-  const lat = message.latitude;
-  const lon = message.longitude;
-  fetch(
-    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${mapApiconfig}`
-  )
-    .then((res) => {
-      console.log(11, res.json());
-    })
-    .then((result) => {
-      console.log(14, result);
-    });
+  // const mapApiconfig = {
+  //   token: "AIzaSyAee8zZhNBTeGuHNIClNS7ZSPqxG-a3Wgo",
+  // };
+  const { latitude, longitude, address, title } = message;
+  // fetch(
+  //   `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${mapApiconfig}`
+  // )
+  //   .then((res) => {
+  //     console.log(11, res.json());
+  //   })
+  //   .then((result) => {
+  //     console.log(14, result);
+  //   });
   return client.replyMessage(replyToken, {
     type: "text",
-    text: `title: ${message.title}\naddress: ${message.address}\nlatitude: ${message.latitude}\nlongitude: ${message.longitude}`,
+    text: `title: ${title}\naddress: ${address}\nlatitude: ${latitude}\nlongitude: ${longitude}`,
   });
 };
 
