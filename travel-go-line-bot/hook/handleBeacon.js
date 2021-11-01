b94b0f62dcd6a78ad47d117c1e07d2ca16b541d4
1072030 <46060471+1072030@users.noreply.github.com>
2021-11-01 16:09:26 +0800

    #update beacon settings

--- travel-go-line-bot/hook/handleBeacon.js
+++ travel-go-line-bot/hook/handleBeacon.js
@@ -3,8 +3,15 @@ const firestore = require("../config/firestore");
 const handleBeacon = async (source, replyToken) => {
   const firestoreData = await firestore.collection("Shop").get();
   firestoreData.forEach(async (doc) => {
-    if (doc.data().beaconId === "50") {
-      console.log(doc.data());
+    switch (doc.data().beaconId) {
+      case "50":
+        //firestore.collection("Shop").doc(doc.id).set({});
+        return await client.replyMessage(replyToken, {
+          type: "text",
+          text: `hello`,
+        });
+      case "72":
+        break;
     }
   });
   //{ hwid: '0154a2ec89', type: 'enter', dm: '50' } beacon Info
@@ -13,14 +20,18 @@ const handleBeacon = async (source, replyToken) => {
   //     text: `hello`,
   //   });
   // {
-  //   password: 'JD',
-  //   userList: {
-  //     JIIWEOJ234234: {
-  //       name: '王大明',
-  //       displayName: '王大明明',
-  //       Img: 'imgURL',
-  //       createTime: [Timestamp]
-  //     }
-  //  },
+  //     beaconId: '50',
+  //     coordinates: GeoPoint { _latitude: 24.6994863, _longitude: 121.0570292 },
+  //     password: 'JD',
+  //     userList: {
+  //       JIIWEOJ234234: {
+  //         Img: 'imgURL',
+  //         name: '王大明',
+  //         createTime: [Timestamp],
+  //         displayName: '王大明明'
+  //       }
+  //     },
+  //     name: '粄條'
+  //   }
 };
 module.exports = handleBeacon;
