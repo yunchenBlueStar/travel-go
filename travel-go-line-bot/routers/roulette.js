5eb5e2b118d943125195bac2df9f7719075738bf
1072030 <46060471+1072030@users.noreply.github.com>
2021-11-03 12:19:10 +0800

    update roulette

--- travel-go-line-bot/routers/roulette.js
+++ travel-go-line-bot/routers/roulette.js
@@ -6,54 +6,18 @@ const router = express.Router();
 router.post("/getResult", async (req, res) => {
   const Random = Math.floor(Math.random() * 9 + 1);
   let tempExp = 0;
-
+  console.log(req.body);
   await realtimeDatabase
     .ref(`${req.body.userId}`)
     .get()
     .then((snapshot) => {
       if (snapshot.exists()) {
         tempExp = snapshot.val().exp;
-      } else {
       }
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  if (tempExp === undefined) {
-    await realtimeDatabase.ref(`${req.body.userId}`).update({
-      exp: 0,
-    });
-  }
-  if (Random == 1) {
-    await realtimeDatabase.ref(`${req.body.userId}`).update({
-      exp: (tempExp += 15),
-    });
-    return res.send({
-      status: "success",
-      data: Random,
-    });
-  } else if (Random == 2) {
-    await realtimeDatabase.ref(`${req.body.userId}`).update({
-      exp: (tempExp += 15),
-    });
-    return res.send({
-      status: "success",
-      data: Random,
-    });
-  } else if (Random == 5) {
-    await realtimeDatabase.ref(`${req.body.userId}`).update({
-      exp: (tempExp += 10),
-    });
-    return res.send({
-      status: "success",
-      data: Random,
-    });
-  } else if (Random == 8) {
-    await realtimeDatabase.ref(`${req.body.userId}`).set({
-      exp: (tempExp += 100),
-    });
-    return res.send({
-      status: "success",
-      data: Random,
-    });
-  }
   res.send({
     status: "success",
     data: Random,
