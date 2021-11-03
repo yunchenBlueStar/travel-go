d5a3144413bc3d57c273a3335d755a63063ff273
1072030 <46060471+1072030@users.noreply.github.com>
2021-11-03 12:53:57 +0800

    test json form

--- travel-go-line-bot/app.js
+++ travel-go-line-bot/app.js
@@ -21,8 +21,8 @@ const config = {
 
 const app = express();
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 app.use("/roulette", roulette);
 app.post("/callback", (req, res) => {
   Promise.all(req.body.events.map(handleEvent))
