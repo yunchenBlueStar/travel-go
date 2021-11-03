73ceb24ed08cc6a1c707603d120b43d037bbb14f
呂允丞 <h1253567@gmail.com>
2021-11-03 14:49:25 +0800

    距離 Line Push Message 測試

--- travel-go-line-bot/hook/handleLocation.js
+++ travel-go-line-bot/hook/handleLocation.js
@@ -5,26 +5,31 @@ const shopData = [
     name: "噹好吃鹹豬肉",
     latitude: 24.6996187,
     longitude: 121.0553976,
+    address: "新竹縣北埔鄉廟前街20之10號",
   },
   {
     name: "福美軒",
     latitude: 24.7010344,
     longitude: 121.0550215,
+    address: "新竹縣北埔鄉南興街132號",
   },
   {
     name: "北埔廟前粄條",
     latitude: 24.6996739,
     longitude: 121.0553805,
+    address: "新竹縣北埔鄉北埔街13號",
   },
   {
     name: "姜太公柿餅",
     latitude: 24.6994756,
     longitude: 121.0554569,
+    address: "新竹縣北埔鄉廟前街24號",
   },
   {
     name: "哈客愛擂茶",
     latitude: 24.7008924,
     longitude: 121.0551534,
+    address: "新竹縣北埔鄉中豐路30號",
   },
 ];
 let caculateDistance = [];
@@ -50,6 +55,7 @@ const handleLocation = (message, replyToken) => {
     caculateDistance.push({
       name: shopData[i].name,
       distance: distance,
+      address: shopData[i].address,
     });
   }
   // return client.replyMessage(replyToken, {
@@ -61,13 +67,71 @@ const handleLocation = (message, replyToken) => {
   caculateDistance.sort((a, b) => a.distance - b.distance);
   caculateDistance.forEach((x, index) => {
     returnMessage.push({
-      type: "text",
-      text: `第${index + 1}個\店家: ${x.name}\ndistance: ${x.distance}`,
+      type: "bubble",
+      hero: {
+        type: "image",
+        url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+        size: "full",
+        aspectRatio: "20:13",
+        aspectMode: "cover",
+      },
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [
+          {
+            type: "text",
+            text: x.name,
+            weight: "bold",
+            size: "xl",
+            contents: [],
+          },
+          {
+            type: "box",
+            layout: "vertical",
+            spacing: "sm",
+            margin: "lg",
+            contents: [
+              {
+                type: "box",
+                layout: "baseline",
+                spacing: "sm",
+                contents: [
+                  {
+                    type: "text",
+                    text: "店家地址",
+                    size: "sm",
+                    color: "#AAAAAA",
+                    flex: 2,
+                    contents: [],
+                  },
+                  {
+                    type: "text",
+                    text: x.address,
+                    size: "sm",
+                    color: "#666666",
+                    flex: 5,
+                    wrap: true,
+                    contents: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
     });
   });
   console.log(56, returnMessage);
   // return client.replyMessage(replyToken, returnMessage);
-  client.replyMessage(replyToken, returnMessage);
+  client.replyMessage(replyToken, {
+    type: "flex",
+    altText: "旅Go 周邊店家",
+    contents: {
+      type: "carousel",
+      contents: [returnMessage],
+    },
+  });
   caculateDistance = [];
   returnMessage = [];
 };
