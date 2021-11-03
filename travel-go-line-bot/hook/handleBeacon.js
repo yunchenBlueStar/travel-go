228f26658d4cc6c737a8e420af39dab8c912087f
1072030 <46060471+1072030@users.noreply.github.com>
2021-11-03 11:26:47 +0800

    update

--- travel-go-line-bot/hook/handleBeacon.js
+++ travel-go-line-bot/hook/handleBeacon.js
@@ -4,16 +4,14 @@ const admin = require("firebase-admin"); //arrayUnion 一定要從admin崁入
 const handleBeacon = async (event, replyToken) => {
   const firestoreData = await firestore.collection("Shop").get();
   firestoreData.forEach(async (doc) => {
+    const user = {
+      userId: event.source.userId,
+      creatTime: event.timestamp,
+    };
     switch (doc.data().beaconId) {
       case "50":
-        const user = {
-          userId: event.source.userId,
-          creatTime: event.timestamp,
-        };
-        const isExist = await searchUserData(doc.id, event.source.userId);
-
+        const isExist = await SearchUserData(doc.id, event.source.userId);
         if (!isExist) {
-          console.log(isExist);
           await firestore
             .collection("Shop")
             .doc(doc.id)
@@ -25,44 +23,19 @@ const handleBeacon = async (event, replyToken) => {
         break;
     }
   });
-  //{ hwid: '0154a2ec89', type: 'enter', dm: '50' } beacon Info
-  //   return await client.replyMessage(replyToken, {
-  //     type: "text",
-  //     text: `hello`,
-  //   });
-  // {
-  //     beaconId: '50',
-  //     coordinates: GeoPoint { _latitude: 24.6994863, _longitude: 121.0570292 },
-  //     password: 'JD',
-  //     userList: {
-  //       JIIWEOJ234234: {
-  //         Img: 'imgURL',
-  //         name: '王大明',
-  //         createTime: [Timestamp],
-  //         displayName: '王大明明'
-  //       }
-  //     },
-  //     name: '粄條'
-  //   }
 };
-const searchUserData = async (docId, userId) => {
+const CompareBeacon = async () => {};
+const SearchUserData = async (docId, userId) => {
   const firestoreData = await firestore
     .collection("Shop")
     .doc(`${docId}`)
     .get();
   let isExist = false;
-  console.log(firestoreData.data().userList);
-  console.log(firestoreData.data().userList.length);
   firestoreData.data().userList.forEach((doc) => {
-    console.log(doc.userId);
     if (userId === doc.userId) {
       isExist = true;
     }
   });
-  if (isExist) {
-    return true;
-  } else {
-    return false;
-  }
+  return isExist;
 };
 module.exports = handleBeacon;
