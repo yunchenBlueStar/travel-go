b5b8e26c18577132e537c8f06f31fae32b5f56eb
1072030 <46060471+1072030@users.noreply.github.com>
2021-11-04 16:24:02 +0800

    update menu fix bug

--- travel-go-line-bot/app.js
+++ travel-go-line-bot/app.js
@@ -25,7 +25,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/roulette", roulette);
-app.use("/", pointcard);
+app.use("/pointcard", pointcard);
 app.post("/callback", (req, res) => {
   Promise.all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
