860e6327fe1ec152c9115468d849cbd5dd61a267
1072030 <46060471+1072030@users.noreply.github.com>
2021-11-04 15:54:02 +0800

    #change folder position

--- travel-go-line-bot/routers/pointcard.js
+++ travel-go-line-bot/routers/pointcard.js
@@ -1,8 +1,10 @@
 const serviceAccount = require("../config/travel-rego-firebase-adminsdk-5yu3d-60f544b0da.json");
-const { realtimeDatabase } = require("../config/firestore");
+const { firestore } = require("../config/firestore");
 const express = require("express");
 const router = express.Router();
 router.post("/pointcard", (req, res) => {
-  console.log("connect successful");
+  const firestoreData = await firestore.collection("Users").get();
+  //   firestoreData.forEach(async (doc) => {
+  //   })
 });
 module.exports = router;
