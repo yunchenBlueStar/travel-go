8627b962886966f8c5a4e58cebc6f6da163335b7
1072030 <46060471+1072030@users.noreply.github.com>
2021-11-05 13:01:10 +0800

    #update point card check position

--- travel-go-liff/pointcard-liff/public/script/index.js
+++ travel-go-liff/pointcard-liff/public/script/index.js
@@ -18,8 +18,42 @@ function initializeLiff(myLiffId) {
         .catch((err) => {
           console.log(err);
         });
+      const checkPosition = [
+        [92, 100],
+        [92, 220],
+        [163, 50],
+        [163, 161],
+        [163, 271],
+        [323, 116],
+        [323, 208],
+        [391, 70],
+        [391, 163],
+        [391, 252],
+        [567, 108],
+        [567, 204],
+        [643, 67],
+        [643, 158],
+        [643, 250],
+      ];
+      addConfirm(checkPosition[1][0], checkPosition[1][1]);
+      // fetch("https://travel-go-line-bot-2.herokuapp.com/pointcard/", {
+      //   method: "POST",
+      //   body: JSON.stringify(data),
+      //   headers: {
+      //     "Access-Control-Allow-Origin": "*",
+      //     "Content-Type": "application/json",
+      //   },
+      // })
+      //   .then((res) => {
+      //     return res.json();
+      //   })
+      //   .then((result) => {
+      //     console.log(result);
+      //   });
     });
 }
-function myFunction() {
-  alert("click");
+function addConfirm(top, left) {
+  $("#main").append(
+    `<div class="confirm" style="top: ${top}px; left: ${left}px"></div>`
+  );
 }
