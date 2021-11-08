a89798395e2e8b99cb6d034420dc4f658be113c6
1072030 <46060471+1072030@users.noreply.github.com>
2021-11-08 16:01:13 +0800

    test reply

--- travel-go-line-bot/hook/handleBeacon.js
+++ travel-go-line-bot/hook/handleBeacon.js
@@ -39,20 +39,22 @@ const handleBeacon = async (event, replyToken) => {
                 userList: admin.firestore.FieldValue.arrayUnion(user), //寫入陣列
               });
             await updateUserData(event.source.userId, doc.data().beaconId);
-            await client.replyMessage(replyToken, {
-              type: "image",
-              originalContentUrl:
-                "https://firebasestorage.googleapis.com/v0/b/travel-rego.appspot.com/o/mud_2-2.png?alt=media&token=b4162216-dbca-4342-a2ff-f4c9ded966df",
-              previewImageUrl:
-                "https://firebasestorage.googleapis.com/v0/b/travel-rego.appspot.com/o/mud_2-2.png?alt=media&token=b4162216-dbca-4342-a2ff-f4c9ded966df",
-            });
-            await client.replyMessage(replyToken, {
-              type: "image",
-              originalContentUrl:
-                "https://firebasestorage.googleapis.com/v0/b/travel-rego.appspot.com/o/mud_1.png?alt=media&token=e493d1d6-6179-4375-a96e-010ebca4a2f0",
-              previewImageUrl:
-                "https://firebasestorage.googleapis.com/v0/b/travel-rego.appspot.com/o/mud_1.png?alt=media&token=e493d1d6-6179-4375-a96e-010ebca4a2f0",
-            });
+            await client.replyMessage(replyToken, [
+              {
+                type: "image",
+                originalContentUrl:
+                  "https://firebasestorage.googleapis.com/v0/b/travel-rego.appspot.com/o/mud_2-2.png?alt=media&token=b4162216-dbca-4342-a2ff-f4c9ded966df",
+                previewImageUrl:
+                  "https://firebasestorage.googleapis.com/v0/b/travel-rego.appspot.com/o/mud_2-2.png?alt=media&token=b4162216-dbca-4342-a2ff-f4c9ded966df",
+              },
+              {
+                type: "image",
+                originalContentUrl:
+                  "https://firebasestorage.googleapis.com/v0/b/travel-rego.appspot.com/o/mud_1.png?alt=media&token=e493d1d6-6179-4375-a96e-010ebca4a2f0",
+                previewImageUrl:
+                  "https://firebasestorage.googleapis.com/v0/b/travel-rego.appspot.com/o/mud_1.png?alt=media&token=e493d1d6-6179-4375-a96e-010ebca4a2f0",
+              },
+            ]);
           }
           break;
         case "41":
